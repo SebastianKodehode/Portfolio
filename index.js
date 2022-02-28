35ba41ff266315bf733bb505977628402ab72fce
SebastianKodehode <Sebastian.kodehode@gmail.com>
2022-02-28 14:22:44 +0100

    style update

--- index.js
+++ index.js
@@ -11,3 +11,34 @@ overlay.addEventListener('click', function() {
   burgerMenu.classList.remove('close')
   overlay.classList.remove('overlay')
 })
+
+
+
+// ===== Fade in div ===== //
+
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px -100px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver
+(function(
+  entries, 
+  appearOnScroll) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add('appear');
+        appearOnScroll.unobserve(entry.target);
+      }
+    })
+  }, 
+  appearOptions);
+
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  })
+
